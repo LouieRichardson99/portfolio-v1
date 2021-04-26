9b52e5a6a486ffd44d9b1b9aa10c37ad4475569c
Louie Richardson <lrich99@protonmail.com>
2021-04-26 12:56:29 +0100

    Removed async attribute from Bootstrap CDN

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -14,22 +14,24 @@ export default function post({ post }) {
         desc={post.data.blog_excerpt[0].text}
       />
       <main className="w-11/12 sm:w-3/4 md:max-w-3xl mx-auto mt-8 sm:mt-16 font-body">
-        <div className={markdownStyles["markdown"]}>
-          {RichText.render(post.data.blog_title)}
-        </div>
-        <div className="text-gray-700 mb-6 sm:mb-10 text-center -mt-8">
-          <BlogDate dateString={post.first_publication_date} />
-        </div>
-        <Image
-          className="rounded-xl mb-8 sm:mb-10"
-          src={post.data.hero_image.url}
-          alt={post.data.hero_image.alt}
-          width={post.data.hero_image.dimensions.width}
-          height={post.data.hero_image.dimensions.height}
-        />
-        <div className={markdownStyles["markdown"]}>
-          {RichText.render(post.data.blog_content)}
-        </div>
+        <article>
+          <div className={markdownStyles["markdown"]}>
+            {RichText.render(post.data.blog_title)}
+          </div>
+          <div className="text-gray-700 mb-6 sm:mb-10 text-center -mt-8">
+            <BlogDate dateString={post.first_publication_date} />
+          </div>
+          <Image
+            className="rounded-xl mb-8 sm:mb-10"
+            src={post.data.hero_image.url}
+            alt={post.data.hero_image.alt}
+            width={post.data.hero_image.dimensions.width}
+            height={post.data.hero_image.dimensions.height}
+          />
+          <div className={markdownStyles["markdown"]}>
+            {RichText.render(post.data.blog_content)}
+          </div>
+        </article>
       </main>
     </>
   );
