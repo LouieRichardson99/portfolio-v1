7691c139e91afac3be9a4370ab29a61f3a26ba2a
Louie Richardson <lrich99@protonmail.com>
2021-05-13 13:30:14 +0100

    Changed width of blog panels for mobile view

--- components/BlogPanel.js
+++ components/BlogPanel.js
@@ -4,20 +4,18 @@ import Image from "next/image";
 export default function BlogPanel(props) {
   return (
     <Link href={`/blog/${props.blogSlug}`}>
-        <div className="font-body text-gray-800 mx-3 my-6 cursor-pointer">
-          <Image
-            className="rounded-xl"
-            width={props.blogImageWidth}
-            height={props.blogImageHeight}
-            src={props.blogHeroImage}
-            alt={props.blogAltText}
-          />
-          <p className="text-sm mt-2 px-1">Posted: {props.blogDate}</p>
-          <div className="text-xl my-2 px-1 font-semibold">
-            {props.blogTitle}
-          </div>
-          <div className="w-10/12 px-1">{props.blogExcerpt}</div>
-        </div>  
+      <div className="font-body text-gray-800 mx-auto my-6 cursor-pointer w-full md:w-11/12">
+        <Image
+          className="rounded-xl"
+          width={props.blogImageWidth}
+          height={props.blogImageHeight}
+          src={props.blogHeroImage}
+          alt={props.blogAltText}
+        />
+        <p className="text-sm mt-2 px-1">Posted: {props.blogDate}</p>
+        <div className="text-xl my-2 px-1 font-semibold">{props.blogTitle}</div>
+        <div className="px-1 w-full">{props.blogExcerpt}</div>
+      </div>
     </Link>
   );
 }
