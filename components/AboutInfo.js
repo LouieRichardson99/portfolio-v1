536542a48cd26c0fe6796920d150c0bf67adae14
Louie Richardson <lrich99@protonmail.com>
2021-05-16 18:33:41 +0100

    Made some adjustments to about me paragraph. Also changed the HTML layout to make sure the Meta component is outside the main HTML element

--- components/AboutInfo.js
+++ components/AboutInfo.js
@@ -3,14 +3,15 @@ export default function AboutInfo() {
     <div className="mt-4">
       <p className="mb-3">
         I am a passionate individual always wanting to learn more about software
-        engineering and computer science. I considers myself as a quick learner
-        and I am always setting out challenges to learn different programming
-        concepts and to find a more efficient way of solving problems.
+        engineering and computer science. I consider myself a quick learner
+        always setting out challenges to learn different programming concepts
+        and to find a more efficient way of solving problems.
       </p>
       <p className="mb-3">
-        Coding is my passion and have made it my mission to become an expert in
-        this field. I have been building up my skills and a portfolio to
-        showcase my knowledge for developing efficient web applications.
+        Coding is my passion and I have made it my mission to become an expert
+        in this field. I have been developing my skills and building up a
+        portfolio to showcase my knowledge for developing efficient web
+        applications.
       </p>
       <p className="mb-3">
         I love both front-end and back-end development, using technologies like
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,27 +7,29 @@ import Meta from "../../components/Meta";
 
 export default function blog({ blogs }) {
   return (
-    <main className="font-sans mt-10 text-gray-800">
+    <>
       <Meta title="Blog" desc="I talk about anything tech related!" />
-      <h1 className="font-semibold mb-4 text-2xl text-center">Blog</h1>
-      <div className="blog-grid">
-        {blogs.results.map((blog) => {
-          return (
-            <BlogPanel
-              key={blog.uid}
-              blogHeroImage={blog.data.hero_image.url}
-              blogImageWidth={blog.data.hero_image.dimensions.width}
-              blogImageHeight={blog.data.hero_image.dimensions.height}
-              blogDate={<BlogDate dateString={blog.first_publication_date} />}
-              blogTitle={RichText.render(blog.data.blog_title)}
-              blogAltText={blog.data.hero_image.alt}
-              blogExcerpt={RichText.render(blog.data.blog_excerpt)}
-              blogSlug={blog.uid}
-            />
-          );
-        })}
-      </div>
-    </main>
+      <main className="font-sans mt-10 text-gray-800">
+        <h1 className="font-semibold mb-4 text-2xl text-center">Blog</h1>
+        <div className="blog-grid">
+          {blogs.results.map((blog) => {
+            return (
+              <BlogPanel
+                key={blog.uid}
+                blogHeroImage={blog.data.hero_image.url}
+                blogImageWidth={blog.data.hero_image.dimensions.width}
+                blogImageHeight={blog.data.hero_image.dimensions.height}
+                blogDate={<BlogDate dateString={blog.first_publication_date} />}
+                blogTitle={RichText.render(blog.data.blog_title)}
+                blogAltText={blog.data.hero_image.alt}
+                blogExcerpt={RichText.render(blog.data.blog_excerpt)}
+                blogSlug={blog.uid}
+              />
+            );
+          })}
+        </div>
+      </main>
+    </>
   );
 }
 
