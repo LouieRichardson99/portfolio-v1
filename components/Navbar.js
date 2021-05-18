229be729b84208a7134fcdc86e82682a4a293f18
Louie Richardson <lrich99@protonmail.com>
2021-05-18 16:28:15 +0100

    Bootstrap Nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,21 +2,45 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="max-w-7xl mx-auto">
-      <ul className="flex justify-end mt-6 mr-6">
-        <li className="hover:text-red-600">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="ml-6 hover:text-red-600">
-          <Link href="/#projects">Projects</Link>
-        </li>
-        <li className="ml-6 hover:text-red-600">
-          <Link href="/blog">Blog</Link>
-        </li>
-        <li className="ml-6 hover:text-red-600">
-          <Link href="/#contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-md navbar-light bg-white">
+      <div className="container-fluid text-center sm:text-left text-gray-800">
+        <div></div>
+        <button
+          className="navbar-toggler top-2 relative right-2 mb-3"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav justify-end w-full sm:top-3 relative sm:right-3">
+            <li className="nav-item">
+              <Link href="/">
+                <a className="nav-link sm:mr-5">Home</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/#projects">
+                <a className="nav-link sm:mr-5">Projects</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/blog">
+                <a className="nav-link sm:mr-5">Blog</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/#contact">
+                <a className="nav-link sm:mr-5">Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
